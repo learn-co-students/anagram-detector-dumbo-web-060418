74adc0d346c3918d490155c95219a6dcab976bc5
mark sauer-utley <mark.utley.713@gmail.com>
2018-06-08 18:12:29 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+require 'pry'
+class Anagram
+
+  attr_accessor :keyword
+
+  def initialize(keyword)
+    @keyword = keyword
+  end
+
+  def match(words_arr)
+    words_arr.select do |word|
+      word.chars.sort == @keyword.chars.sort
+    end
+  end
+
+end
